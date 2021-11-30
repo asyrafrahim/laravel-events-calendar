34c439e2bcb4b6ca3cb053bd597173007da1bc0b
Asyraf Rahim <asyrafboyz@gmail.com>
2021-11-30 21:28:53 +0800

    update model fillable

--- app/Models/CrudEvents.php
+++ app/Models/CrudEvents.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class CrudEvents extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'event_name', 
+        'event_start', 
+        'event_end'
+    ];    
 }
